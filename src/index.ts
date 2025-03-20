9809a8e4f0b57e7c8ae1a1c9e65bfd6cafd0b7f7
Mirzaqosimova Farzona <mirzaqosimovafarzona@gmail.com>
2025-03-20 13:47:58 +0500

    check by ip removed check by cookie added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // Install dependencies: express, pg, knex, cors, dotenv, @types/express
 // Run: npm install express pg knex cors dotenv @types/express
-
 import express, { Request, Response } from "express";
 import knex from "knex";
 import cors from "cors";
@@ -8,6 +7,7 @@ import dotenv from "dotenv";
 import { nanoid } from "nanoid";
 import { Status } from "./const";
 import axios from "axios";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,8 +17,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-//CHATGPT ETDI serverda agar ishlamasa shuni qoy db
-app.set("trust proxy", true);
+app.use(cookieParser());
 
 // Initialize Knex
 const db = knex({
@@ -46,10 +45,21 @@ const db = knex({
         .onDelete("CASCADE")
         .notNullable();
       table.string("ip_address").notNullable();
+      table.string("user_id").notNullable();
     });
     console.log("Table 'links' created");
   }
 })();
+
+app.use((req, res, next) => {
+  let userId = req.cookies?.user_id;
+  if (!userId) {
+    userId = nanoid(8); // Generate a unique identifier
+    res.cookie("user_id", userId, { maxAge: 31536000000, httpOnly: true }); // Store for 1 year
+  }
+  req.body["userId"] = userId;
+  next();
+});
 
 // API to create a new short link
 app.post("/api/create", async (req: Request, res: Response): Promise<void> => {
@@ -111,24 +121,26 @@ app.get(
   async (req: Request<{ short_id: string }>, res: Response): Promise<void> => {
     try {
       const { short_id } = req.params;
+      const userId = req.body.userId;
       const ip_address =
         req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-      const private_ip = req.socket.remoteAddress;
 
-      console.log(
-        `Public IP: ${ip_address}, Private IP: ${private_ip}, Socket: ${req.socket.remoteAddress}`
-      );
-
+      console.log(userId, ip_address);
       const link = await db("links")
         .where({ short_id, status: Status.ACTIVE })
         .first();
+
+      if (!userId) {
+        res.status(404).json({ error: "Oops something went wrong" });
+        return;
+      }
 
       if (!link) {
         res.status(404).json({ error: "Link not found" });
         return;
       }
       const hasVisited = await db("link_visitors")
-        .where({ link_id: link.id, ip_address })
+        .where({ link_id: link.id, user_id: userId })
         .first();
 
       if (hasVisited) {
@@ -139,6 +151,7 @@ app.get(
       await db.transaction(async (trx) => {
         return trx("link_visitors")
           .insert({
+            user_id: userId,
             ip_address,
             link_id: link.id,
           })
